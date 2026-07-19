0c13e9da2fed5aa511eb7358dc4135f6e3cb0e86
Nadia-2005-code <oak200511@gmail.com>
2026-07-19 12:40:17 +0100

    Update main.gd

--- main.gd
+++ main.gd
@@ -1,11 +1,24 @@
 extends Node
 
+var boardSize: int
+var columnWidth: int
+var gridPos: Vector2i
+var player: int
+var winner: int
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	boardSize = $board.texture.get_width()
+	columnWidth = boardSize / 7
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			gridPos = Vector2i(event.position/columnWidth)
+			print(gridPos)
+		
+		
+func new_game():
+	player = 1
+	winner = 0
