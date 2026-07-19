4683f06ab5f29f0ee2a9218f51d15bef7eba70a4
Nadia-2005-code <oak200511@gmail.com>
2026-07-19 13:29:57 +0100

    updated game logic to track the filling of each column

--- main.gd
+++ main.gd
@@ -1,24 +1,46 @@
 extends Node
 
+var playableSize: float
 var boardSize: int
-var columnWidth: int
+var columnWidth: float
 var gridPos: Vector2i
 var player: int
 var winner: int
-
-
+var boardData: Array
+var columnFill: Array
+var movesCount: int
+var marginSize: int = 50
+var boardStart_x:int
+var adjusted_x: int
 
 func _ready() -> void:
-	boardSize = $board.texture.get_width()
-	columnWidth = boardSize / 7
+	boardSize = $Board.texture.get_width()
+	playableSize = boardSize - (marginSize*2)
+	columnWidth = playableSize / 7
+	var boardStart_x = $Board.global_position.x
+	new_game()
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			gridPos = Vector2i(event.position/columnWidth)
-			print(gridPos)
-		
+			adjusted_x = event.position.x - boardStart_x - marginSize
+			var col = floor(adjusted_x / columnWidth)
+			col = clamp(col, 0, 6)
+			if col >= 0 and col < 7 and columnFill[col] < 6:
+				columnFill[col] += 1
+				print(columnFill)
 		
 func new_game():
 	player = 1
 	winner = 0
+	boardData = [
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0]
+		]
+	columnFill = [0,0,0,0,0,0,0]
+	movesCount = 0
